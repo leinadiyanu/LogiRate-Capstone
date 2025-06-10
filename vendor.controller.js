fbdcedfe333032889f8c2f9a5cf485f740db289d
deborah747 <ajulodeborah20@gmail.com>
2025-06-10 19:06:20 +0100

    Add files via upload

---
+++ vendor.controller.js
@@ -0,0 +1,52 @@
+const Vendor = require('../models/vendor.model');
+
+exports.createVendor = async (req, res, next) => {
+  try {
+    const vendor = await Vendor.create({ ...req.body, createdBy: req.user.id });
+    res.status(201).json(vendor);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAllVendors = async (req, res, next) => {
+  try {
+    const vendors = await Vendor.find().populate('createdBy', 'email name');
+    res.status(200).json(vendors);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getVendorById = async (req, res, next) => {
+  try {
+    const vendor = await Vendor.findById(req.params.id).populate('createdBy', 'email name');
+    if (!vendor) return res.status(404).json({ message: 'Vendor not found' });
+
+    res.status(200).json(vendor);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateVendor = async (req, res, next) => {
+  try {
+    const vendor = await Vendor.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!vendor) return res.status(404).json({ message: 'Vendor not found' });
+
+    res.status(200).json(vendor);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteVendor = async (req, res, next) => {
+  try {
+    const vendor = await Vendor.findByIdAndDelete(req.params.id);
+    if (!vendor) return res.status(404).json({ message: 'Vendor not found' });
+
+    res.status(200).json({ message: 'Vendor deleted' });
+  } catch (err) {
+    next(err);
+  }
+};
